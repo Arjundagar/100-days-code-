22f9603f9b76d102c7269a20a8e79aa28cf02df0
Arjun Dagar <arjundagar2006@gmail.com>
2025-09-15 22:08:17 +0530

    Add files via upload

---
+++ exe.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int r=7;
+    float v=3.14*r*r;
+    printf("the area is :%f",v);
+    return 0;
+}
+
---
+++ ques1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x=10;
+    x = x + 5;
+    printf("the sum of :%d",x);
+    return 0;
+ }
